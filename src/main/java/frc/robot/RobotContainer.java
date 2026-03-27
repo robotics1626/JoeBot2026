bb763cdf40c50861256105b0c3dc0e97fd85b7de
robotics1626 <robotics1626@gmail.com>
2026-03-26 21:50:13 -0500

    RAH

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,14 +19,12 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.AlignHeadingToHub;
 import frc.robot.commands.AutoAimShooter;
@@ -202,24 +200,14 @@ public class RobotContainer {
         "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
 
     autoChooser.addOption(
-    "BBAll",
-    new SequentialCommandGroup(
-
-        autoAlignHeadingToHub.until(autoAlignHeadingToHub.isLookingAtHub()),
-
-        new WaitUntilCommand(auto_autoAimShooter::isAtTargetRpm)
-            .deadlineFor(auto_autoAimShooter),
-
-        auto_autoAimShooter.withTimeout(2.0),
-
-        new ParallelDeadlineGroup(
-            new WaitCommand(5.0),
-            auto_autoAimShooter,
-            indexer.indexFlow()
-        )
-
-    )
-);
+        "BBAll",
+        new SequentialCommandGroup(
+            autoAlignHeadingToHub.until(autoAlignHeadingToHub.isLookingAtHub()),
+            new WaitUntilCommand(auto_autoAimShooter::isAtTargetRpm)
+                .deadlineFor(auto_autoAimShooter),
+            auto_autoAimShooter.withTimeout(2.0),
+            new ParallelDeadlineGroup(
+                new WaitCommand(5.0), auto_autoAimShooter, indexer.indexFlow())));
 
     // Configure the button bindings
     configureButtonBindings();
