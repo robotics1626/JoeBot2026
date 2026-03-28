a8da0d89f38f1dcde54b6748ff7c365557cec3cc
wagu <whataffuf@gmail.com>
2026-03-28 07:57:39 -0400

    Potential up close shot fix

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -75,10 +75,10 @@ public final class ShooterConstants {
       .86, 1.76, 2.18, 2.60, 2.84, 2.94, 3.19, 3.70, 4.72, 5.01, 5.73
     };
 
-    public static final double[] kPivotPosition = {11, 18, 25, 28, 19, 22, 24, 31, 33, 34.3, 37.5};
+    public static final double[] kPivotPosition = {13, 18, 25, 28, 19, 22, 24, 31, 33, 34.3, 37.5};
 
     public static final double[] kShooterTargetRpm = {
-      2800, 3200, 2930, 3150, 3263, 3400, 3050, 3566, 3750, 3800, 3950
+      2900, 3200, 2930, 3150, 3263, 3400, 3050, 3566, 3750, 3800, 3950
     };
   }
 }
